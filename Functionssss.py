72e71da86053c0a029526c2094702783a944e176
raghuvir-einredner <raghuvircodes786@gmail.com>
2026-02-02 12:32:51 +0530

    adding function topic codes

---
+++ Functionssss.py
@@ -0,0 +1,23 @@
+# function is reusable block of code which gives output acc to input
+'''def sum(a,b):
+    sum = a+b
+    print (sum)
+
+x=int(input("enter first number : "))
+y=int(input("enter second number : "))
+sum(x,y)'''
+
+# if you return value of function we will have to call it inside print fucntion to get our result on screen
+'''def triple(c):
+    y=c*3
+    return y
+
+j=int(input("enter number : "))
+print(triple(j))'''
+
+# using function with default parameter , when we dont have to give any input ,just print
+'''def patternprintt( ):
+    for i in range(5):
+        print("Hello",end=" ") #default ending is changed
+
+patternprintt()'''
